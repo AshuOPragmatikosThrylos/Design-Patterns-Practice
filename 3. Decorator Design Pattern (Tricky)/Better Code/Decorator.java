19cf8f4c873d03f857fcebb599ed49975d32434a
Ashutosh <kumarashutosh3627405@gmail.com>
2024-08-24 16:40:20 +0530

    proxy DP done

--- 3. Decorator Design Pattern (Tricky)/Better Code/Decorator.java
+++ 3. Decorator Design Pattern (Tricky)/Better Code/Decorator.java
@@ -15,3 +15,5 @@ public abstract class Decorator implements BaseTypeInterface { // decorator is-a
 
 // either implement the abstract methods of the interface or declare yourself abstract class
 // Decorator should be an abstract class cuz I want its child classes to mandatorily implement their own operation() with addOn operations
+
+// abstract class might not have any abstrat method. But if a class has even 1 abstract method then the class has to be declared abstract
