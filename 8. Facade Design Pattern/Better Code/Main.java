ab307f9ca62c2947152ed0f0d69371891fdd27ff
Ashutosh <kumarashutosh3627405@gmail.com>
2024-10-21 16:47:36 +0530

    facade DP done

---
+++ 8. Facade Design Pattern/Better Code/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    // Client code
+    public static void main(String[] args) {
+        FacadeOfFacades facade = new FacadeOfFacades();
+        // Use case 3: Merge facades into one for the client
+        facade.mergedFacadeFunction();
+    }
+}
